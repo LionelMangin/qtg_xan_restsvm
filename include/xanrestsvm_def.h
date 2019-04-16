edaa4dcbdff9f52bb06d2b8da7abe78c4d66e4b5
b25 <>
2019-04-16 16:08:39 +0000

    B25: add content-type to allow header

--- include/xanrestsvm_def.h
+++ include/xanrestsvm_def.h
@@ -9,8 +9,29 @@
 // Write here all types definitions and constants.
 #include "constants.h"
 
-namespace xanrestsvm {
-	extern const string kz_qobuzOrigin; ///< *.qobuz.com
+namespace xanrestsvm
+{
+  ///@name json
+  //@{
+  static const char * const kz_h_origin = "origin"; ///< header - origin
+  static const char * const kz_h_date = "date"; ///< header - date
+  static const char * const kz_h_server = "server"; ///< header - server
+  static const char * const kz_h_accessControlMaxAge = "access-control-max-age"; ///< header - access-control-max-age
+  static const char * const kz_h_accessControlAllowMethods = "access-control-allow-methods"; ///< header - access-control-allow-methods
+  static const char * const kz_h_accessControlAllowHeaders = "access-control-allow-headers"; ///< header - access-control-allow-headers
+  static const char * const kz_h_accessControlExposeHeaders = "access-control-expose-headers"; ///< header - access-control-expose-headers
+  static const char * const kz_h_accessControlAllowOrigin = "access-control-allow-origin"; ///< header - access-control-allow-origin
+  static const char * const kz_h_accessControlAllowCredentials = "access-control-allow-credentials"; ///< header - access-control-allow-credentials
+  static const char * const kz_v_contentType = "text/plain; charset=UTF-8"; ///< value - text/plain; charset=UTF-8
+  static const char * const kz_v_accessControlMaxAge = "1728000"; ///< value - 1728000
+  static const char * const kz_v_accessControlAllowMethods = "POST, GET, OPTIONS"; ///< value - POST, GET, OPTIONS
+  static const char * const kz_v_accessControlAllowHeaders = "x-app-id, x-user-auth-token, x-app-version, x-api-auth-token, x-socket-id, x-requested-with, content-type"; ///< value - x-app-id, x-user-auth-token, x-app-version, x-api-auth-token, x-socket-id, x-requested-with, content-type
+  static const char * const kz_v_accessControlExposeHeaders = "x-zone, x-language-code"; ///< value - x-zone, x-language-code
+  static const char * const kz_v_accessControlAllowCredentials = "true"; ///< value - true
+  static const char * const kz_v_accessControlAllowAllOrigin = "*"; ///< value - *
+
+  static const string kz_v_accessControlAllowQobuzOrigin("*.qobuz.com"); ///< value - *.qobuz.com
+  //@}
 };
 
 #endif
--- source/xanrestsvm_int.cxx
+++ source/xanrestsvm_int.cxx
@@ -6,10 +6,6 @@
 #include "xanrestsvm_general.h"
 
 // Write XANRESTSVM internal functions here.
-namespace xanrestsvm {
-  const string kz_qobuzOrigin(".qobuz.com"); ///< *.qobuz.com
-};
-
 
 /// @brief pre-processs request.
 bool XAN_RESTSVM::preProcessRequest()
@@ -27,7 +23,7 @@ bool XAN_RESTSVM::preProcessRequest()
   bool lb_found = false;
   for (ubyte4 l4_idx = 1; l4_idx <= mcp_bus->httpMessage().headers().size() && !lb_found; ++l4_idx)
   {
-    if (mcp_bus->httpMessage().headers()[l4_idx]->name() == "origin")
+    if (mcp_bus->httpMessage().headers()[l4_idx]->name() == xanrestsvm::kz_h_origin)
     {
       mcp_ctx->originHeader(mcp_bus->httpMessage().headers()[l4_idx]->value());
       lb_found = true;
@@ -53,38 +49,38 @@ void XAN_RESTSVM::forceResponseBusFields()
     byte4 l4_optionIdx = 0;
     mcp_bus->httpMessage().headers().bus(0);
     mcp_bus->httpMessage().headers()[++l4_optionIdx]->name(kz_headerContentType);
-    mcp_bus->httpMessage().headers()[l4_optionIdx]->value("text/plain; charset=UTF-8");
-    mcp_bus->httpMessage().headers()[++l4_optionIdx]->name("date");
+    mcp_bus->httpMessage().headers()[l4_optionIdx]->value(xanrestsvm::kz_v_contentType);
+    mcp_bus->httpMessage().headers()[++l4_optionIdx]->name(xanrestsvm::kz_h_date);
     mcp_bus->httpMessage().headers()[l4_optionIdx]->value(mt_now.dateFormat("%a, %d %b %Y %H:%M:%S GMT")); // cf. https://tools.ietf.org/html/rfc2616#section-3.3
-    mcp_bus->httpMessage().headers()[++l4_optionIdx]->name("server");
+    mcp_bus->httpMessage().headers()[++l4_optionIdx]->name(xanrestsvm::kz_h_server);
     mcp_bus->httpMessage().headers()[l4_optionIdx]->value(mcp_cfg->server());
-    mcp_bus->httpMessage().headers()[++l4_optionIdx]->name("access-control-max-age");
-    mcp_bus->httpMessage().headers()[l4_optionIdx]->value("1728000");
+    mcp_bus->httpMessage().headers()[++l4_optionIdx]->name(xanrestsvm::kz_h_accessControlMaxAge);
+    mcp_bus->httpMessage().headers()[l4_optionIdx]->value(xanrestsvm::kz_v_accessControlMaxAge);
   }
 
   // add header values
   byte4 l4_idx = mcp_bus->httpMessage().headers().size();
-  mcp_bus->httpMessage().headers()[++l4_idx]->name("access-control-allow-methods");
-  mcp_bus->httpMessage().headers()[l4_idx]->value("POST, GET, OPTIONS");
-  mcp_bus->httpMessage().headers()[++l4_idx]->name("access-control-allow-headers");
-  mcp_bus->httpMessage().headers()[l4_idx]->value("x-app-id, x-user-auth-token, x-app-version, x-api-auth-token, x-socket-id, x-requested-with");
-  mcp_bus->httpMessage().headers()[++l4_idx]->name("access-control-expose-headers");
-  mcp_bus->httpMessage().headers()[l4_idx]->value("x-zone, x-language-code");
+  mcp_bus->httpMessage().headers()[++l4_idx]->name(xanrestsvm::kz_h_accessControlAllowMethods);
+  mcp_bus->httpMessage().headers()[l4_idx]->value(xanrestsvm::kz_v_accessControlAllowMethods);
+  mcp_bus->httpMessage().headers()[++l4_idx]->name(xanrestsvm::kz_h_accessControlAllowHeaders);
+  mcp_bus->httpMessage().headers()[l4_idx]->value(xanrestsvm::kz_v_accessControlAllowHeaders);
+  mcp_bus->httpMessage().headers()[++l4_idx]->name(xanrestsvm::kz_h_accessControlExposeHeaders);
+  mcp_bus->httpMessage().headers()[l4_idx]->value(xanrestsvm::kz_v_accessControlExposeHeaders);
 
 
   // origin
-  if (mcp_ctx->originHeader().size() > xanrestsvm::kz_qobuzOrigin.size() &&
-      mcp_ctx->originHeader().compare(mcp_ctx->originHeader().length() - xanrestsvm::kz_qobuzOrigin.length(), xanrestsvm::kz_qobuzOrigin.length(), xanrestsvm::kz_qobuzOrigin) == 0)
+  if (mcp_ctx->originHeader().size() > xanrestsvm::kz_v_accessControlAllowQobuzOrigin.size() &&
+      mcp_ctx->originHeader().compare(mcp_ctx->originHeader().length() - xanrestsvm::kz_v_accessControlAllowQobuzOrigin.length(), xanrestsvm::kz_v_accessControlAllowQobuzOrigin.length(), xanrestsvm::kz_v_accessControlAllowQobuzOrigin) == 0)
   {
-    mcp_bus->httpMessage().headers()[++l4_idx]->name("access-control-allow-origin");
+    mcp_bus->httpMessage().headers()[++l4_idx]->name(xanrestsvm::kz_h_accessControlAllowOrigin);
     mcp_bus->httpMessage().headers()[l4_idx]->value(mcp_ctx->originHeader());
-    mcp_bus->httpMessage().headers()[++l4_idx]->name("access-control-allow-credentials");
-    mcp_bus->httpMessage().headers()[l4_idx]->value("true");
+    mcp_bus->httpMessage().headers()[++l4_idx]->name(xanrestsvm::kz_h_accessControlAllowCredentials);
+    mcp_bus->httpMessage().headers()[l4_idx]->value(xanrestsvm::kz_v_accessControlAllowCredentials);
   }
   else
   {
-    mcp_bus->httpMessage().headers()[++l4_idx]->name("access-control-allow-origin");
-    mcp_bus->httpMessage().headers()[l4_idx]->value("*");
+    mcp_bus->httpMessage().headers()[++l4_idx]->name(xanrestsvm::kz_h_accessControlAllowOrigin);
+    mcp_bus->httpMessage().headers()[l4_idx]->value(xanrestsvm::kz_v_accessControlAllowAllOrigin);
   }
 
   // stat user data
@@ -92,7 +88,6 @@ void XAN_RESTSVM::forceResponseBusFields()
   {
     mcp_ctx->statUserData(mcp_ctx->statUserData() + ';' + mcp_bus->app_id());
   }
-if (isVerbose()) cout << "[" << ka_funcName << " (" << __LINE__ << ")] " << "mcp_ctx->statUserData() :" << mcp_ctx->statUserData() << ":" << endl;
 }
 
 
